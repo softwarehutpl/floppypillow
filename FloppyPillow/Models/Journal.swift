7698fedd1bc15e76ba201e27f502253fb52fe941
Michał Karpowicz <michal.karpowicz@softwarehut.pl>
2016-12-06 10:29:48 +0100

    Create synchronization service

---
+++ FloppyPillow/Models/Journal.swift
@@ -0,0 +1,15 @@
+//
+//  Journal.swift
+//  FloppyPillow
+//
+//  Created by Michał Karpowicz on 06/12/16.
+//  Copyright © 2016 SoftwareHut. All rights reserved.
+//
+
+import Foundation
+
+class Journal {
+    
+    
+    
+}
---
+++ FloppyPillow/Services/SynchronizationService.swift
@@ -0,0 +1,21 @@
+//
+//  SynchronizationService.swift
+//  FloppyPillow
+//
+//  Created by Michał Karpowicz on 06/12/16.
+//  Copyright © 2016 SoftwareHut. All rights reserved.
+//
+
+import Foundation
+
+class SynchronizationService : NSObject {
+    
+    func getJournals(failure: (Error) -> Void, success: ([Journal]) -> Void) {
+        
+    }
+    
+    func synchronize(journal: Journal, failure: (Error) -> Void, success: () -> Void) {
+        
+    }
+    
+}
